fb0837e8090aac8fa5118000937faaff4db6a585
Sakib1286 <najmussakib603@gmail.com>
2025-09-03 01:16:38 +0600

    first commit

---
+++ lib/onboarding_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body:Column(
+        children: [
+            Expanded(
+              flex: 2,
+              child: Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/onboarding.png'),
+                  fit: BoxFit.cover)),),
+            ),
+          Expanded(
+            child: Container(
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Premium Cars\nEnjoy the Luxury",
+                    style: TextStyle(color: Colors.black54,fontSize: 32,fontWeight: FontWeight.bold,fontStyle:FontStyle.italic),),
+                  SizedBox(height: 10,),
+                  Text("Premium and prestige car daily rental\nExperience the thrill at a lower price",
+                    style: TextStyle(color: Colors.black54,fontSize: 16),),
+                  SizedBox(height: 20,),
+
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(backgroundColor:Colors.white,foregroundColor: Colors.black),
+                        child: Text("Let\'s Go")),
+                  )
+                ],
+              ),
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
